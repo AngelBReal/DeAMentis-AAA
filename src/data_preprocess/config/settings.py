01e233d8a248687b1d4a3a42b06600f43cb403e0
AngelBReal <130816518+AngelBReal@users.noreply.github.com>
2025-05-27 21:51:38 -0700

    Update settings.py

--- src/data_preprocess/config/settings.py
+++ src/data_preprocess/config/settings.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-# Forza la raíz del proyecto con un path absoluto
 BASE_DIR = Path(__file__).resolve().parents[3]
 RAW_DATA_DIR = BASE_DIR / "data" / "raw"
 INTERIM_DATA_DIR = BASE_DIR / "data" / "interim"
